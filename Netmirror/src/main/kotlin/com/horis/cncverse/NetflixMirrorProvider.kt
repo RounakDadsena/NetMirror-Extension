904cecc97d878963e49ffccb39da6045bb10b249
Sushan Khatiwada <sushan.khatiwada2064@gmail.com>
2026-08-01 09:42:28 +0545

    changed back urls

--- Netmirror/src/main/kotlin/com/horis/cncverse/NetflixMirrorProvider.kt
+++ Netmirror/src/main/kotlin/com/horis/cncverse/NetflixMirrorProvider.kt
@@ -45,7 +45,7 @@ class NetflixMirrorProvider : MainAPI() {
 
     private val embedHeaders = mapOf(
         "Accept" to "application/json",
-        "Referer" to "https://net77.cc/",
+        "Referer" to "https://videodownloader.site/",
         "User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
     )
 
