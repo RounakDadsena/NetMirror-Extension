96c78bf368b0536bcde890f536376d72b7a5c983
Rounak Dadsena <rounak.dadsena@gmail.com>
2026-08-26 21:47:07 +0530

    Rename plugin to NetMirrorFix

--- NetMirrorFix/build.gradle.kts
+++ NetMirrorFix/build.gradle.kts
@@ -17,6 +17,7 @@ cloudstream {
     language = "hi"
     // All of these properties are optional, you can safely remove them
 
+    
     description = "Netflix, PrimeVideo, Disney+ Hotstar Contents in Multiple Languages"
     authors = listOf("Sushan64")
 
